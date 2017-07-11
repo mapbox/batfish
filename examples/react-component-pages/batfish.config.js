2d37559cf4412ecc49571dd870ce5d69d8dcd932
John Furrow <john.furrow@mapbox.com>
2017-07-11 15:52:07 -0700

    Remove siteOrigin from config

--- examples/react-component-pages/batfish.config.js
+++ examples/react-component-pages/batfish.config.js
@@ -7,7 +7,6 @@ module.exports = () => {
     externalStylesheets: [
       'https://api.mapbox.com/mapbox-assembly/v0.13.0/assembly.min.css'
     ],
-    siteOrigin: 'https://www.mapbox.com',
     wrapperPath: path.join(__dirname, './src/components/wrapper.js')
   };
 };
