e24820f655d8566591223f13c308283a749cfaa4
David Clark <dclark@mapbox.com>
2017-07-16 22:38:37 -0500

    WIP findMatchingRoute tests

--- src/find-matching-route.js
+++ src/find-matching-route.js
@@ -2,17 +2,27 @@
 
 const batfishContext = require('batfish/context');
 
-// An array of regular expressions that are used to check whether a URL path
-// matches one of the routes.
-const pathRegExps = batfishContext.routes.reduce((result, route) => {
-  // Pages with internal routing aren't just exact matches.
-  const ending = route.internalRouting ? '(/.+)?$' : '$';
-  // ? because the last slash is optional
-  result[route.path] = new RegExp(
-    `^${route.path.replace(/\//g, '[/]')}?${ending}`
-  );
-  return result;
-}, {});
+/**
+ * Returns an array of regular expressions that are used to check whether a URL path
+ * matches one of the routes.
+ *
+ * @param {Array<{ path: string }>} routes
+ * @return {Object} - Keys are paths, values are regular expressions.
+ */
+function createPathRegExps(routes) {
+  return routes.reduce((result, route) => {
+    // Pages with internal routing aren't just exact matches.
+    const ending = route.internalRouting ? '(/.+)?$' : '$';
+    // ? because the last slash is optional
+    result[route.path] = new RegExp(
+      `^${route.path.replace(/\//g, '[/]')}?${ending}`
+    );
+    return result;
+  }, {});
+}
+
+let pathRegExpsCache;
+let urlPathsCache;
 
 /**
  * Find the route data that matches a URL path.
@@ -21,24 +31,45 @@ const pathRegExps = batfishContext.routes.reduce((result, route) => {
  * @param {Object} [options]
  * @param {boolean} [options.notFound=false] - If true, returns a special
  *   not-found route if no route is found.
+ * @param {boolean} [options.useCache=true]
  * @return {?Object} - The matching route, or `undefined` or a not-found route
  *   if no matching route exists.
  */
 function findMatchingRoute(urlPath, options = {}) {
-  for (let i = 0, l = batfishContext.routes.length; i < l; i++) {
+  const useCache = options.useCache === undefined ? true : options.useCache;
+
+  if (useCache && urlPathsCache[urlPath]) {
+    return urlPathsCache[urlPath];
+  }
+
+  let pathRegExps;
+  if (useCache && pathRegExpsCache) {
+    pathRegExps = pathRegExpsCache;
+  } else {
+    pathRegExps = createPathRegExps(batfishContext.routes);
+    pathRegExpsCache = pathRegExps;
+  }
+
+  let result;
+
+  for (let i = 0; i < batfishContext.routes.length; i++) {
     const route = batfishContext.routes[i];
     if (pathRegExps[route.path].test(urlPath)) {
-      return route;
+      result = route;
+      break;
     }
   }
 
-  if (options.notFound) {
-    return {
+  if (!result && options.notFound) {
+    result = {
       path: urlPath,
       getPage: batfishContext.getNotFoundPage,
       internalRouting: false
     };
   }
+
+  urlPathsCache[urlPath] = result;
+  return result;
 }
 
 module.exports = findMatchingRoute;
---
+++ test/find-matching-route.test.js
@@ -0,0 +1,20 @@
+// 'use strict';
+//
+// const findMatchingRoute = require('../src/find-matching-route');
+//
+// // jest.setMock('batfish/context', () => {
+// //   return {
+// //     routes: [
+// //       { path: '/foo/' },
+// //       { path: '/foo/bar/' },
+// //       { path: '/baz/' },
+// //       { path: '/baz/bing/' }
+// //     ]
+// //   }
+// // });
+// //
+// // jest.mock('batfish/context');
+//
+// describe('findMatchingRoute', () => {
+//
+// });
