016cd997b82c36741164d06674401ec835581847
John Furrow <john.furrow@mapbox.com>
2017-07-25 17:26:54 -0700

    Prevent scrolling to top under certain conditions

--- src/router.js
+++ src/router.js
@@ -90,7 +90,9 @@ class Router extends React.PureComponent {
     }
     this.changePage(targetLocation, {
       pushState: true,
-      scrollToTop: true
+      scrollToTop:
+        window.location.pathname !== targetLocation.pathname ||
+        !targetLocation.hash
     });
   }
 
