17f6ce2d2218eed7ae60db2bb41e07758b10c9f0
John Furrow <john.furrow@mapbox.com>
2017-09-06 16:32:34 -0700

    Create screen-reader content based on title element

--- src/webpack/public/focus-content-entry.js
+++ src/webpack/public/focus-content-entry.js
@@ -1,12 +1,44 @@
 // @flow
 
-function focusContentEntry() {
-  const contentEntry = document.querySelector('h1, h2, h3, h4, h5, h6, main');
+const contentEntryElement = document.createElement('div');
+contentEntryElement.setAttribute(
+  'style',
+  `border: 0; clip: rect(0 0 0 0); height: 1px; margin: -1px; overflow: hidden; padding: 0; position: absolute; width: 1px; white-space: nowrap;`
+);
+contentEntryElement.setAttribute('tabindex', '-1');
 
-  if (contentEntry) {
-    contentEntry.setAttribute('tabindex', '-1');
-    contentEntry.focus();
+function prependContentEntryElement() {
+  if (!document.body.contains(contentEntryElement)) {
+    const firstBodyChild = document.body.firstChild;
+
+    if (firstBodyChild) {
+      document.body.insertBefore(contentEntryElement, firstBodyChild);
+    } else {
+      document.body.appendChild(contentEntryElement);
+    }
   }
+}
+
+function updateContentEntryElementText() {
+  const titleElement = document.querySelector('title');
+
+  if (titleElement) {
+    contentEntryElement.textContent = titleElement.textContent;
+  } else {
+    contentEntryElement.textContent = '';
+  }
+}
+
+function focusContentEntry() {
+  // We requestAnimationFrame in case the browser is busy with another task.
+  // We also use a 100ms timeout because it takes a while for React to render the component tree.
+  requestAnimationFrame(() => {
+    setTimeout(() => {
+      updateContentEntryElementText();
+      prependContentEntryElement();
+      contentEntryElement.focus();
+    }, 100);
+  });
 }
 
 export { focusContentEntry };
