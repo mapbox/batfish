300f9f57e59943fa7f687f39db4bab80ded67564
David Clark <dclark@mapbox.com>
2018-05-23 11:15:02 -0700

    Add 404 to miscellany example

---
+++ examples/miscellany/src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="viewport-full flex-parent flex-parent--center-main flex-parent--center-cross">
+        <div className="flex-child txt-h1">404: Page not found</div>
+      </div>
+    );
+  }
+}
