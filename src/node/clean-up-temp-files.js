a4ad4febfe79bc439a94784f09cf84aa4db2b4bf
danswick <dan.swick@gmail.com>
2019-01-31 17:50:38 -0800

    pacify flow

--- src/node/clean-up-temp-files.js
+++ src/node/clean-up-temp-files.js
@@ -3,32 +3,20 @@
 
 const path = require('path');
 const del = require('del');
-const pTry = require('p-try');
 
 const cleanUpTempFiles = (
   batfishConfig: BatfishConfiguration
-): Promise<void> => {
-  const assetsDirectory = path.join(
-    batfishConfig.outputDirectory,
-    batfishConfig.publicAssetsPath
+): Promise<any> => {
+  return del(
+    ['assets.json', 'static-render-pages.js', 'static-render-pages.js.map'],
+    {
+      force: true,
+      cwd: path.join(
+        batfishConfig.outputDirectory,
+        batfishConfig.publicAssetsPath
+      )
+    }
   );
-
-  const assetsJsonPath = path.join(assetsDirectory, 'assets.json');
-  const renderStaticPagesPath = path.join(
-    assetsDirectory,
-    'static-render-pages.js'
-  );
-  const renderStaticPagesMapPath = path.join(
-    assetsDirectory,
-    'static-render-pages.js.map'
-  );
-
-  return pTry(() => {
-    return del(
-      [assetsJsonPath, renderStaticPagesPath, renderStaticPagesMapPath],
-      { force: true }
-    );
-  });
 };
 
 module.exports = cleanUpTempFiles;
