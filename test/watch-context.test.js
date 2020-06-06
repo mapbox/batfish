6b5774df5f799ef4fcbd8becd6ede36a0f8e8560
David Clark <dclark@mapbox.com>
2020-06-05 17:10:46 -0700

    Fix bug crashing watch mode
    Closes #337.

--- test/watch-context.test.js
+++ test/watch-context.test.js
@@ -21,18 +21,13 @@ jest.mock('../src/node/write-context-module', () => {
 
 describe('watchContext', () => {
   let onError;
-  let afterCompilation;
 
   beforeEach(() => {
     onError = jest.fn();
-    afterCompilation = jest.fn();
   });
 
   test('creates a watcher', () => {
-    watchContext(
-      { pagesDirectory: 'src/pages/' },
-      { onError, afterCompilation }
-    );
+    watchContext({ pagesDirectory: 'src/pages/' }, { onError });
     expect(chokidar.watch).toHaveBeenCalledWith('**/*.{js,md}', {
       ignoreInitial: true,
       cwd: 'src/pages/'
@@ -40,52 +35,40 @@ describe('watchContext', () => {
   });
 
   test('handles errors', () => {
-    watchContext(
-      { pagesDirectory: 'src/pages/' },
-      { onError, afterCompilation }
-    );
+    watchContext({ pagesDirectory: 'src/pages/' }, { onError });
     expect(chokidar.watcher.on).toHaveBeenCalledWith('error', onError);
   });
 
   test('rebuilds the context module on change events', () => {
     const batfishConfig = { pagesDirectory: 'src/pages/' };
-    watchContext(batfishConfig, { onError, afterCompilation });
+    watchContext(batfishConfig, { onError });
     expect(chokidar.watcher.on.mock.calls[0][0]).toBe('change');
     // Manually trigger a change event.
     chokidar.watcher.on.mock.calls[0][1]();
     expect(writeContextModule).toHaveBeenCalledWith(batfishConfig);
-    return Promise.resolve().then(() => {
-      expect(afterCompilation).toHaveBeenCalled();
-    });
   });
 
   test('rebuilds the context module on unlink events', () => {
     const batfishConfig = { pagesDirectory: 'src/pages/' };
-    watchContext(batfishConfig, { onError, afterCompilation });
+    watchContext(batfishConfig, { onError });
     expect(chokidar.watcher.on.mock.calls[1][0]).toBe('unlink');
     // Manually trigger a change event.
     chokidar.watcher.on.mock.calls[1][1]();
     expect(writeContextModule).toHaveBeenCalledWith(batfishConfig);
-    return Promise.resolve().then(() => {
-      expect(afterCompilation).toHaveBeenCalled();
-    });
   });
 
   test('rebuilds the context module on add events', () => {
     const batfishConfig = { pagesDirectory: 'src/pages/' };
-    watchContext(batfishConfig, { onError, afterCompilation });
+    watchContext(batfishConfig, { onError });
     expect(chokidar.watcher.on.mock.calls[2][0]).toBe('add');
     // Manually trigger a change event.
     chokidar.watcher.on.mock.calls[2][1]();
     expect(writeContextModule).toHaveBeenCalledWith(batfishConfig);
-    return Promise.resolve().then(() => {
-      expect(afterCompilation).toHaveBeenCalled();
-    });
   });
 
   test('handles errors writing the context module', () => {
     const batfishConfig = { pagesDirectory: 'src/pages/' };
-    watchContext(batfishConfig, { onError, afterCompilation });
+    watchContext(batfishConfig, { onError });
     expect(chokidar.watcher.on.mock.calls[0][0]).toBe('change');
     // Set up the error.
     const error = new Error();
@@ -94,7 +77,6 @@ describe('watchContext', () => {
     chokidar.watcher.on.mock.calls[0][1]();
     expect(writeContextModule).toHaveBeenCalledWith(batfishConfig);
     return new Promise((resolve) => setTimeout(resolve, 100)).then(() => {
-      expect(afterCompilation).not.toHaveBeenCalled();
       expect(onError).toHaveBeenCalledWith(error);
     });
   });
